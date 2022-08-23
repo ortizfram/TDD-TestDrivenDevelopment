753e43229eab5e1528703b9abb742ca010ae4097
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-08-23 12:38:54 -0300

    Add files via upload

--- test_calc.py
+++ test_calc.py
@@ -6,13 +6,27 @@ class testCalc(unittest.TestCase):
 
     """first test"""
     def test_add(self):
-         result = calc.add(10, 5)
-         """testing"""
-         self.assetEquals(result, 15)
+         self.assertEqual(calc.add(5, 10), 15)
+         self.assertEqual(calc.add(-1, 1), 0)
+         self.assertEqual(calc.add(-1, -2), -3)
          """to run it in terminal you should go to the directory & type
          > python -m unittest test_calc.py
          if it throws 'OK' it's all good"""
 
+    def test_subtract(self):
+            self.assertEqual(calc.subtract(10, 5), 5)
+            self.assertEqual(calc.subtract(-1, 1), -2)
+            self.assertEqual(calc.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+            self.assertEqual(calc.multiply(10, 5), 50)
+            self.assertEqual(calc.multiply(-1, 1), -1)
+            self.assertEqual(calc.multiply(-1, -1), 1)
+
+    def test_divide(self):
+            self.assertEqual(calc.divide(10, 5), 2)
+            self.assertEqual(calc.divide(-1, 1), -1)
+            self.assertEqual(calc.divide(-1, -1), 1)
 
 """command runs in editor
 this conditional would run the test, even if we type:
