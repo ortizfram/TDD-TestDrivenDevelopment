e72c362ef9fc26ac9514bfd326a0ee5f093132b1
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-08-23 11:43:13 -0300

    Add files via upload

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+import unittest
+import calc #module
+
+"""giving access to testing capabilities"""
+class testCalc(unittest.TestCase): 
+
+    """first test"""
+    def test_add(self):
+         result = calc.add(10, 5)
+         """testing"""
+         self.assetEquals(result, 15)
+         """to run it in terminal you should go to the directory & type
+         > python -m unittest test_calc.py
+         if it throws 'OK' it's all good"""
