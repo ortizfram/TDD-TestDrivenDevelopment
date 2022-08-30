0d47aca581ed3dc7c4f1e1edec5923a966516b0e
melon <ortizfranco48@gmail.com>
2022-08-29 16:54:56 -0800

    fictures json

--- pytest/test_math_func.py
+++ pytest/test_math_func.py
@@ -55,3 +55,28 @@ def test_product_strings():
                         )
 def test_simpadd(num1, num2, result):
     assert math_func.simpadd(num1, num2) == result
+
+"""===============FICTURES json=============================="""
+from math_func import StudentDB
+import pytest
+
+@pytest.fixture(scope='module')
+def db():
+    print('----------setup----------------')
+    db = StudentDB()
+    db.connect('data.json')
+    yield db
+    print('----------teardown----------------')
+    db.close()
+
+def test_scott_data(db):
+    scott_data = db.get_data('Scott')
+    assert scott_data['id'] == 1
+    assert scott_data['name'] == 'Scott'
+    assert scott_data['result'] == 'pass'
+
+def test_mark_data(db):
+    mark_data = db.get_data('Mark')
+    assert mark_data['id'] == 2
+    assert mark_data['name'] == 'Mark'
+    assert mark_data['result'] == 'fail'
